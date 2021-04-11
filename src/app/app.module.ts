b75bffa8d4750e44437c0c29ef2074279f6ad6b9
Hans Kerkhof <hanskerkhof@hanss-mbp.lan>
2021-04-11 22:37:36 +0200

    Upgrade to Angular 11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {NgxMetaTagsModule} from 'ngx-meta-tags';
+import {NgxGoogleAnalyticsModule, NgxGoogleAnalyticsRouterModule} from 'ngx-google-analytics';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
-import { LanguageService } from './_services/language.service';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { ResumeModule } from './resume/resume.module';
-import { NgxGoogleAnalyticsModule, NgxGoogleAnalyticsRouterModule } from 'ngx-google-analytics';
-import { ScrollSpyDirective } from './scroll-spy.directive';
-import { environment } from '../environments/environment';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {SharedModule} from './shared/shared.module';
+import {LanguageService} from './_services/language.service';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {ResumeModule} from './resume/resume.module';
+import {ScrollSpyDirective} from './scroll-spy.directive';
+import {environment} from '../environments/environment';
 
 export function createTranslateLoader(http: HttpClient): TranslateLoader {
   return new TranslateHttpLoader(http, `${environment.api.url}/i18n/`, '.json');
@@ -47,15 +47,6 @@ export function createTranslateLoader(http: HttpClient): TranslateLoader {
         image: 'https://hanskerkhof.nl/assets/images/hanskerkhof-square.jpg'
       }
     ),
-    // NgxMetaTagsModule.forRoot(
-    //   {
-    //     title: 'X Hans kerkhof',
-    //     description: 'X Description',
-    //     author: 'X Author',
-    //     url: 'X url',
-    //     image: 'X Image'
-    //   }
-    // ),
   ],
   providers: [LanguageService],
   bootstrap: [AppComponent]
--- src/app/resume/resume.module.ts
+++ src/app/resume/resume.module.ts
@@ -1,5 +1,9 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { TranslateModule } from '@ngx-translate/core';
+import { MomentModule } from 'ngx-moment';
+
 import { ResumeComponent } from './resume.component';
 import { ResumeSectionsComponent } from './resume-sections/resume-sections.component';
 import { ResumeBasicsComponent } from './sections/resume-basics/resume-basics.component';
@@ -12,33 +16,16 @@ import { ResumeReferencesComponent } from './sections/resume-references/resume-r
 import { ResumeSkillsComponent } from './sections/resume-skills/resume-skills.component';
 import { ResumeVolunteerComponent } from './sections/resume-volunteer/resume-volunteer.component';
 import { ResumeWorkComponent } from './sections/resume-work/resume-work.component';
-import { MomentModule } from 'ngx-moment';
 import { ResumeFileSizePipe } from './pipes/resume-file-size.pipe';
-// import { GalleryModule } from '../gallery/gallery.module';
 import { ResumeService } from './services/resume.service';
-// import {
-//   TranslateLoader,
-//   TranslateModule,
-//   TranslateService,
-// } from '@ngx-translate/core';
-import { Observable, of } from 'rxjs';
-// import nl from '../../assets/i18n/nl.json';
-// import en from '../../assets/i18n/en.json';
 import { ResumeSafeUrlPipe } from './pipes/resume-safe-url.pipe';
 import { ResumeReplacePipe } from './pipes/resume-replace.pipe';
 import { ResumeHighlightsComponent } from './shared/highlights/highlights.component';
 import { ResumeSkillsLogosComponent } from './shared/resume-skills-logos/resume-skills-logos.component';
 import { ResumeDownloadItemComponent } from './shared/resume-download-item/resume-download-item.component';
 import { ResumeMimeToFaIconPipe } from './pipes/resume-mime-to-fa-icon.pipe';
-import { TranslateModule, TranslatePipe } from '@ngx-translate/core';
 import { GalleryModule } from '../gallery/gallery.module';
 import { AppShellNoRenderDirective } from './shared/shell-no-render.directive';
-
-// export class StaticTranslateLoader implements TranslateLoader {
-//   getTranslation(lang: string): Observable<any> {
-//     return of(nl);
-//   }
-// }
 
 @NgModule({
   declarations: [
@@ -68,14 +55,6 @@ import { AppShellNoRenderDirective } from './shared/shell-no-render.directive';
     MomentModule,
     TranslateModule.forChild({ defaultLanguage: 'nl' }),
     GalleryModule,
-    // TranslateModule.forRoot({
-    //   loader: {
-    //     provide: TranslateLoader,
-    //     useClass: StaticTranslateLoader,
-    //   },
-    //   defaultLanguage: 'nl',
-    // }),
-    // TranslateModule.forRoot(),
   ],
   exports: [
     ResumeComponent,
@@ -93,11 +72,6 @@ import { AppShellNoRenderDirective } from './shared/shell-no-render.directive';
   ],
 })
 export class ResumeModule {
-  // constructor(private translateService: TranslateService) {
-  //   translateService.setTranslation('nl', nl, true);
-  //   translateService.setTranslation('en', en, true);
-  // }
-
   static forRoot(): ModuleWithProviders<ResumeModule> {
     return {
       ngModule: ResumeModule,
